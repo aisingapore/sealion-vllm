56238cb538661df6221a25d6e277fe8011766bd8
Ren <62876165+ren-lee@users.noreply.github.com>
2024-11-11 15:15:06 +0800

    Update the test script

--- test.sh
+++ test.sh
@@ -5,10 +5,10 @@
 
 curl http://localhost:8000/v1/completions \
     -H "Content-Type: application/json" \
-    -d '{
-        "model": "llama3-8b-cpt-sea-lionv2.1-instruct",
-        "prompt": "Artificial Intelligence is",
-        "max_tokens": 20,
-        "temperature": 0.8,
-        "repetition_penalty": 1.2
-    }'
+    -d "{
+        \"model\": \"${MODEL_NAME}\",
+        \"prompt\": \"Artificial Intelligence is\",
+        \"max_tokens\": 20,
+        \"temperature\": 0.8,
+        \"repetition_penalty\": 1.2
+    }"
